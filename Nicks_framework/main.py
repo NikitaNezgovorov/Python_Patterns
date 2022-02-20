970d3b618fa76ebcbe1b0742609c3853525225cc
NikitaNezgovorov <75495473+NikitaNezgovorov@users.noreply.github.com>
2022-02-20 12:09:17 +0300

    Исправлены Post запросы

--- Nicks_framework/main.py
+++ Nicks_framework/main.py
@@ -1,9 +1,12 @@
 import os
 import sys
 
+from quopri import decodestring
+
 sys.path.append('../')
 from Nicks_framework.filetypes import file_types
 from Nicks_framework.templator import render
+from Nicks_framework.requests import GetRequests, PostRequests
 
 static_name = 'static'
 
@@ -12,7 +15,7 @@ STATIC_DIR = f'{os.getcwd()}/{static_name}'
 
 class PageNotFound404:
     def __call__(self, request):
-        return '404 WHAT', render('404.html', date=request.get('date', None))
+        return '404 WHAT', render('404.html')
 
 
 class Framework:
@@ -43,7 +46,7 @@ class Framework:
                 status = '200 OK'
 
                 with open(file_path, 'rb') as f:
-                    data = f.read()
+                    static_data = f.read()
                 content_type = ''
 
                 for file_type in file_types:
@@ -52,14 +55,31 @@ class Framework:
             print(content_type)
 
             response_headers = [('Content-type', content_type),
-                                ('Content-Length', str(len(data)))]
+                                ('Content-Length', str(len(static_data)))]
             start_response(status, response_headers)
 
-            return [data]
+            return [static_data]
 
         # добавление закрывающего слеша
         if not path.endswith('/'):
             path = f'{path}/'
+
+        request = {}
+        # Получаем все данные запроса
+        method = environ['REQUEST_METHOD']
+        request['method'] = method
+
+        if method == 'POST':
+            data = PostRequests().get_request_params(environ)
+            request['data'] = Framework.decode_value(data)
+            print(f'Нам пришёл post-запрос: {Framework.decode_value(data)}')
+
+        if method == 'GET':
+            request_params = GetRequests().get_request_params(environ)
+            request['request_params'] = Framework.decode_value(request_params)
+            print(f'Нам пришли GET-параметры:'
+                  f' {Framework.decode_value(request_params)}')
+
 
         # находим нужный контроллер
         # отработка паттерна page controller
@@ -67,7 +87,7 @@ class Framework:
             view = self.routes_lst[path]
         else:
             view = PageNotFound404()
-        request = {}
+
         # наполняем словарь request элементами
         # этот словарь получат все контроллеры
         # отработка паттерна front controller
@@ -77,3 +97,12 @@ class Framework:
         code, body = view(request)
         start_response(code, [('Content-Type', 'text/html')])
         return [body.encode('utf-8')]
+
+    @staticmethod
+    def decode_value(data):
+        new_data = {}
+        for k, v in data.items():
+            val = bytes(v.replace('%', '=').replace("+", " "), 'UTF-8')
+            val_decode_str = decodestring(val).decode('UTF-8')
+            new_data[k] = val_decode_str
+        return new_data
--- views.py
+++ views.py
@@ -3,14 +3,14 @@ from Nicks_framework.templator import render
 
 class Index:
     def __call__(self, request):
-        return '200 OK', render('index.html', date=request.get('date', None))
+        return '200 OK', render('index.html')
 
 
 class About:
     def __call__(self, request):
-        return '200 OK', render('about.html', date=request.get('date', None))
+        return '200 OK', render('about.html')
 
 
 class Contacts:
     def __call__(self, request):
-        return '200 OK', render('contact.html', date=request.get('date', None))
+        return '200 OK', render('contact.html')
